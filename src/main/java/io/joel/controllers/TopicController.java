c2f2f401dcef67d6b1b5bb18dd686a0ad4816635
taddeimania <jtaddei@gmail.com>
2017-07-31 11:14:46 -0400

    Database integration for findAll complete and shows in template

--- src/main/java/io/joel/controllers/TopicController.java
+++ src/main/java/io/joel/controllers/TopicController.java
@@ -1,25 +1,26 @@
 package io.joel.controllers;
 
 import io.joel.models.Topic;
+import io.joel.repositories.TopicRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class TopicController {
 
+    @Autowired
+    private TopicRepositoryImpl repo;
+
     @RequestMapping("/")
     public String index(Model model) {
-        Topic first = new Topic(1, "Java Spring", "Not sure how spring works");
-        Topic second = new Topic(2, "Backend Fundamentals", "NODE ON THE BACKEND?! WHAT!");
-        Topic third = new Topic(3, "React & Redux", "What is a single page app anyway?");
-        ArrayList<Topic> topics = new ArrayList<>();
-        topics.add(first);
-        topics.add(second);
-        topics.add(third);
-        model.addAttribute("topics", topics);
+        List<Topic> allTopics = repo.findAll();
+        model.addAttribute("topics", allTopics);
         return "index";
     }
 
